9f9729a61cdb86cc3cbe0a42a60c1491ad79a2bc
selly361 <seikou615@gmail.com>
2024-01-05 17:10:08 +0000

    Add: `useTheme` hook

---
+++ src/Hooks/useTheme/index.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react'
+
+export const useTheme = () => {
+	const defaultTheme = localStorage.getItem('theme') || (matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light')
+
+	const [theme, setTheme] = useState(defaultTheme)
+
+	useEffect(() => {
+		localStorage.setItem('theme', theme)
+
+		document.documentElement.setAttribute('data-theme', localStorage.getItem('theme'))
+
+	}, [theme])
+
+	function toggle() {
+		setTheme((e) => (e == 'dark' ? 'light' : 'dark'))
+	}
+
+	return {toggle, theme}
+}
+
