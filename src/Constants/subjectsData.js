3f06d208b8566cf7dbae8298c4479dba996d1288
selly361 <seikou615@gmail.com>
2024-01-07 15:37:01 +0000

    Add: `subjectsData` constant

---
+++ src/Constants/subjectsData.js
@@ -0,0 +1,26 @@
+import { AccessibilityIcon, CSSIcon, JSIcon, HTMLIcon } from 'Assets'
+
+const subjectsData = [
+	{
+		Icon: HTMLIcon,
+		label: 'HTML',
+		class: 'subjects__subject-button__icon-container--html'
+	},
+	{
+		Icon: CSSIcon,
+		label: 'CSS',
+		class: 'subjects__subject-button__icon-container--css'
+	},
+	{
+		Icon: JSIcon,
+		label: 'Javascript',
+		class: 'subjects__subject-button__icon-container--javascript'
+	},
+	{
+		Icon: AccessibilityIcon,
+		label: 'Accessibility',
+		class: 'subjects__subject-button__icon-container--accessibility'
+	}
+]
+
+export { subjectsData }
