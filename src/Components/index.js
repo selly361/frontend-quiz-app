c3728971f13252d314be5f64fb75a37fd0cfc6df
selly361 <seikou615@gmail.com>
2024-01-08 01:28:31 +0000

    Update: Export `ThemeButton` component

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,3 +1,4 @@
 export { default as SelectionElement } from './SelectionElement'
 export { default as Subjects } from './Subjects'
 export { default as ProgressBar } from './ProgressBar'
+export { default as ThemeButton } from './ThemeButton'
