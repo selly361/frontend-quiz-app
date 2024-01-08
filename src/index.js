521b124f7d4c833b649f0f056f1c7c48addee61c
selly361 <seikou615@gmail.com>
2024-01-08 01:06:33 +0000

    Update: wrap `App` component with `QuizContextProvider`

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import 'Styles/index.scss';
+import 'Styles/index.scss'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import QuizContextProvider from 'Contexts/QuizContextProvider'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
+
 root.render(
 	<React.StrictMode>
-		<App />
+		<QuizContextProvider>
+			<App />
+		</QuizContextProvider>
 	</React.StrictMode>
 )
 
