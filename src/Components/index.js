0ccf2811910a795126a802c612b263424a29c3d7
codewarswarrior <alieuk61@gmail.com>
2024-01-09 19:06:02 +0000

    fixed error

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-
-=======
 export { default as SelectionElement } from './SelectionElement'
 export { default as Subjects } from './Subjects'
 export { default as ProgressBar } from './ProgressBar'
->>>>>>> d612e2dcf498e90944abaf5b5ac05761f19464fa
 export { default as ThemeButton } from './ThemeButton'
