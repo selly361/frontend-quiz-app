bb749041c6d34b04106373f85a93267b1f525b0c
selly361 <seikou615@gmail.com>
2024-01-12 23:49:55 +0000

    Add: `Intro` component

---
+++ src/Components/Intro/index.jsx
@@ -0,0 +1,11 @@
+function Intro() {
+	return (
+		<article className='intro'>
+			<h2 className='intro__head'>Welcome to the</h2>
+			<h2 className='intro__head--bold'>Frontend Quiz!</h2>
+			<p className='intro__body'>Pick a subject to get started.</p>
+		</article>
+	)
+}
+
+export default Intro
