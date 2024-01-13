365b4d83c3ceb404449cb74cbe09a07dcb1f09f5
selly361 <seikou615@gmail.com>
2024-01-13 20:54:39 +0000

    Update: refactor `useTheme` to debounce `toggle` function

--- src/Hooks/useTheme/index.js
+++ src/Hooks/useTheme/index.js
@@ -1,21 +1,25 @@
 import { useEffect, useState } from 'react'
 
 export const useTheme = () => {
-	const defaultTheme = localStorage.getItem('theme') || (matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light')
+	const defaultTheme =
+		localStorage.getItem('theme') ||
+		(matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light')
 
 	const [theme, setTheme] = useState(defaultTheme)
+	let debounceTimeout
 
 	useEffect(() => {
 		localStorage.setItem('theme', theme)
-
 		document.documentElement.setAttribute('data-theme', theme)
-
 	}, [theme])
 
 	function toggle() {
-		setTheme((e) => (e == 'dark' ? 'light' : 'dark'))
+		clearTimeout(debounceTimeout)
+
+		debounceTimeout = setTimeout(() => {
+			setTheme((currentTheme) => (currentTheme === 'dark' ? 'light' : 'dark'))
+		}, 250)
 	}
 
-	return {toggle, theme}
+	return { toggle, theme }
 }
-
